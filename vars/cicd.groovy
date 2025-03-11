3544dde65685131c4e4aa6c0e51f50a9c4470e4b
ManoharGopi77 <manohar.gopi77@gmail.com>
2025-03-11 16:22:58 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,3 +9,4 @@ def build()
 def deploytomcat(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
